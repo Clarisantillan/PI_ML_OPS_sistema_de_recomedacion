900c0d328abe2fc80233bcf5a19b9af4ce26bf63
Clarisantillan <clarisantillan@icloud.com>
2023-06-23 21:39:10 -0300

    resolucion de conflictos

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import numpy as np
+from fastapi import FastAPI
+
+app = FastAPI(title='Proyecto individual nro 1: Recomendacion de peliculas',
+            description='API de datos y recomendaciones de películas')
